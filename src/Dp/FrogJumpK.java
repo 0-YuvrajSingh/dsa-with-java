27f0420578adbb38755edce52abafb7647803209
Yuvraj Singh <uvi58493@gmail.com>
2025-10-01 16:23:28 +0530

    Added Space Optimization Method As Well

--- src/Dp/FrogJumpK.java
+++ src/Dp/FrogJumpK.java
@@ -11,6 +11,7 @@ public class FrogJumpK {
         for(int i = 0 ; i<n; i++) {
             arr[i] = sc.nextInt();
         }
+        // By Tabulation Method :
         int[] dp = new int[n];
         dp[0] = 0;
         for (int i = 1; i < n; i++) {
@@ -22,7 +23,31 @@ public class FrogJumpK {
                 }
             }
             dp[i] = minSteps;
+
         }
         System.out.println(dp[n-1]);
+        // T.C : O(N*K) S.C:O(N)
+
+        frogJumpKSpace(arr, k);
+    }
+    public static void frogJumpKSpace(int[] arr, int k){
+        int n = arr.length;
+        int[] kSteps = new int[k]; // Circular Buffer
+        kSteps[0] = 0;
+        // Tabulation with Space Optimization worst case has same complexity as Tabulation Method
+        for (int i = 1; i < n; i++) {
+            int minSteps = Integer.MAX_VALUE;
+
+            for (int j = 1; j <= k ; j++) {
+                if(i - j >= 0){
+                    int jump = kSteps[(i-j)%k] + Math.abs(arr[i] - arr[i-j]);
+                    // why % k : if length passed of kSteps then store from 0 again
+                    minSteps = Math.min(minSteps, jump);
+                }
+            }
+            kSteps[i % k] = minSteps;
+        }
+        System.out.println(kSteps[(n-1) % k]);
+        // T.C : O(N*K) S.C : O(K)
     }
 }
